38b8269b152ebde3df6148ca3af890a9542c5f8b
Viviana Garcia <vivianandrea4@gmail.com>
2021-03-18 23:16:08 -0400

    transferred portfolio site code

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+
+
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.navBarMobile');
+let bod = document.querySelector('.container')
+
+hamburger.addEventListener('click', function () {
+    // hamburger.classList.toggle('isactive');
+    menu.classList.toggle('active');
+});
+
+mybutton = document.querySelector("logo");
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+document.querySelector(".test").onclick = function (event) {
+    console.log('this is clicked')
+}
+let testLinked = document.querySelectorAll(".test")
+
+for (let testLink of testLinked) {
+    testLink.onclick = function (event) {
+        console.log('this is clicked')
+    }
+
+}
